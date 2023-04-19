9d87334541d79167c93e64e33279c8e7184e2968
Anton Nevero <nevero.anton@gmail.com>
2023-04-19 09:31:38 +0000

    Add route table

--- terransible/main.tf
+++ terransible/main.tf
@@ -10,6 +10,9 @@ resource "aws_vpc" "mtc_vpc" {
     tags = {
         Name = "mpc_vpc-${random_id.random.dec}"
     }
+    lifecycle {
+        create_before_destroy = true
+    }
 }
 
 resource "aws_internet_gateway" "mtc_internet_gateway" {
@@ -19,3 +22,19 @@ resource "aws_internet_gateway" "mtc_internet_gateway" {
         Name = "mtc_igw-${random_id.random.dec}"
     }
 }
+
+resource "aws_route_table" "mtc_public_rt" {
+    vpc_id = aws_vpc.mtc_vpc.id
+    
+    tags = {
+        Name = "mtc-public"
+    }
+}
+
+resource "aws_route" "default_route"{
+    route_table_id = aws_route_table.mtc_public_rt.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mtc_internet_gateway.id
+}
+
+
