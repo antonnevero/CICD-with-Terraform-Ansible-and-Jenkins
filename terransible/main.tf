ea5407cd049036d6c4634465ed5262f94a60760d
Anton Nevero <nevero.anton@gmail.com>
2023-04-20 09:03:52 +0000

    Add locals

--- terransible/main.tf
+++ terransible/main.tf
@@ -1,3 +1,7 @@
+locals {
+    azs = data.aws_availability_zones.available.names
+}
+
 data "aws_availability_zones" "available" {}
 
 resource "random_id" "random" {
@@ -52,7 +56,7 @@ resource "aws_subnet" "mtc_public_subnet" {
     vpc_id = aws_vpc.mtc_vpc.id
     cidr_block = var.public_cidrs[count.index]
     map_public_ip_on_launch = true
-    availability_zone = data.aws_availability_zones.available.names[count.index]
+    availability_zone = local.azs[count.index]
     
     tags = {
         Name = "mtc-public-${count.index + 1}"
@@ -63,7 +67,8 @@ resource "aws_subnet" "mtc_private_subnet" {
     count = length(var.private_cidrs)
     vpc_id = aws_vpc.mtc_vpc.id
     cidr_block = var.private_cidrs[count.index]
-    availability_zone = data.aws_availability_zones.available.names[count.index]
+    map_public_ip_on_launch = false
+    availability_zone = local.azs[count.index]
     
     tags = {
         Name = "mtc-private-${count.index + 1}"
--- terransible/variables.tf
+++ terransible/variables.tf
@@ -10,5 +10,5 @@ variable public_cidrs {
 
 variable private_cidrs {
     type = list(string)
-    default = ["10.124.5.0/24", "10.124.7.0/24"]
+    default = ["10.124.2.0/24", "10.124.4.0/24"]
 }
