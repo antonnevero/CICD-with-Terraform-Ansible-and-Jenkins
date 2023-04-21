65fee0b67a6d36ac9c85598309925d399df12b5f
Anton Nevero <nevero.anton@gmail.com>
2023-04-21 10:10:19 +0000

    Add userdata

--- terransible/compute.tf
+++ terransible/compute.tf
@@ -9,17 +9,29 @@ data "aws_ami" "server_ami" {
     }
 }
 
+resource "random_id" "mtc_node_id" {
+    byte_length = 2
+    count = var.main_instance_count
+}
+
+resource "aws_key_pair" "mtc_auth" {
+    key_name = var.key_name
+    public_key = file(var.public_key_path)
+}
+
 resource "aws_instance" "mtc_main" {
+    count = var.main_instance_count
     instance_type = var.main_instance_type
     ami = data.aws_ami.server_ami.id
-    # key_name = ""
+    key_name = aws_key_pair.mtc_auth.id
     vpc_security_group_ids = [aws_security_group.mtc_sg.id]
-    subnet_id = aws_subnet.mtc_public_subnet[0].id
+    subnet_id = aws_subnet.mtc_public_subnet[count.index].id
+    user_data = templatefile("./main-userdata.tpl", {new_hostname = "mtc-main-${random_id.mtc_node_id[count.index].dec}"})
     root_block_device {
         volume_size = var.main_vol_size
     }
     
     tags = {
-        Name = "mtc-main"
+        Name = "mtc-main-${random_id.mtc_node_id[count.index].dec}"
     }
 }
