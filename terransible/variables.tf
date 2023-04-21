b3f9379715b5209853adb23ebda804aee603d8f6
Anton Nevero <nevero.anton@gmail.com>
2023-04-21 08:52:48 +0000

    Add instance

--- terransible/variables.tf
+++ terransible/variables.tf
@@ -7,3 +7,13 @@ variable access_ip {
     type = string
     default = "94.232.72.131/32"
 }
+
+variable main_instance_type {
+    type = string
+    default = "t2.micro"
+}
+
+variable main_vol_size {
+    type = number
+    default = 8
+}
